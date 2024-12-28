50c73beccbfe573103174a6d2a53ccb61c3770ae
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-12-28 23:17:48 +0630

    feat: add event service

--- EventManagementSystem.Domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace EventManagementSystem.Domain
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ EventManagementSystem.Domain/Features/EventService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventManagementSystem.Database.AppDbContextModels;
+
+namespace EventManagementSystem.Domain.Features;
+
+public class EventService
+{
+    private readonly AppDbContext _context;
+
+    public EventService(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public IEnumerable<Event> GetAllEvents()
+    {
+        return _context.Events.ToList();
+    }
+
+    public void CreateEvent(Event newEvent)
+    {
+        _context.Events.Add(newEvent);
+        _context.SaveChanges();
+    }
+}
