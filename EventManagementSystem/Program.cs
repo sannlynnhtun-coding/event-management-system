d1a847cf2eadb849b9ae480a9c97387addc7ee00
Sann Lynn Htun <sannlynnhtun.developer@gmail.com>
2024-12-29 23:09:14 +0630

    feat: add service for register

--- EventManagementSystem/Program.cs
+++ EventManagementSystem/Program.cs
@@ -1,6 +1,11 @@
+using EventManagementSystem.Database.AppDbContextModels;
 using EventManagementSystem.Domain.Features;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<EventService>();
 
